3d091b3d72bff74f1a8f1ffa648317c23faa718c
Andriy1409 <krandriy14904@gmail.com>
2023-11-09 16:07:08 +0200

    Make WPF

---
+++ MoneyManagerApp/MoneyManagerApp/Sing_Up.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyManagerApp.Presentation
+{
+    /// <summary>
+    /// Interaction logic for Sing_Up.xaml
+    /// </summary>
+    public partial class Sing_Up : Window
+    {
+        public Sing_Up()
+        {
+            InitializeComponent();
+        }
+    }
+}
