738d53f683c4e8210cdb47cec5f2ae699d4ceb14
Vetal-Koz <kozvitalik567@gmail.com>
2023-12-12 23:28:49 +0200

    Implement main logic of My_profile page

--- MoneyManagerApp/MoneyManagerApp/My_Profile.xaml.cs
+++ MoneyManagerApp/MoneyManagerApp/My_Profile.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MoneyManagerApp.DAL.Helpers;
+using MoneyManagerApp.Presentation.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,69 @@ namespace MoneyManagerApp.Presentation
         public My_Profile()
         {
             InitializeComponent();
+            
+            LoadInformationProfile();
+        }
+
+        private void LoadInformationProfile()
+        {
+            byte[] imageData;
+            using (var dbcontext = new ApplicationContext())
+            {
+                User user = dbcontext.Users.Where(u => u.UsersId == CurrentUser.UserId).FirstOrDefault();
+                if (user != null)
+                {
+                    UsernameTextBlock.Text = user.UsersName;
+                    EmailTextBlock.Text = user.UsersEmail;
+                    byte[] imageBytes = user.UsersPhoto;
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = new MemoryStream(imageBytes);
+                    bitmap.EndInit();
+
+                    // Показати зображення на WPF
+                    MyImage.Source = bitmap;
+
+                }
+            }
+        }
+
+        static string ConvertByteaToString(byte[] byteaValue)
+        {
+            // Перетворення byte[] в рядок за допомогою BitConverter
+            string stringValue = BitConverter.ToString(byteaValue).Replace("-", "");
+            return stringValue;
+        }
+
+        static byte[] ConvertByteaToBytes(string byteaData)
+        {
+            // Видаляємо подвійні бекслеші та розділяємо рядок на пари символів
+            byteaData = byteaData.Replace("\\x", "");
+            string[] bytePairs = SplitString(byteaData, 2);
+
+            // Конвертуємо кожну пару в байт та складаємо в масив байтів
+            byte[] byteArray = new byte[bytePairs.Length];
+            for (int i = 0; i < bytePairs.Length; i++)
+            {
+                byteArray[i] = Convert.ToByte(bytePairs[i], 16);
+            }
+
+            return byteArray;
+        }
+
+        static string[] SplitString(string str, int chunkSize)
+        {
+            int stringLength = str.Length;
+            int numOfChunks = stringLength / chunkSize + (stringLength % chunkSize == 0 ? 0 : 1);
+            string[] chunks = new string[numOfChunks];
+
+            for (int i = 0; i < numOfChunks; i++)
+            {
+                int length = Math.Min(chunkSize, stringLength - i * chunkSize);
+                chunks[i] = str.Substring(i * chunkSize, length);
+            }
+
+            return chunks;
         }
 
 
